6e1bb10c8045cec656ac0b08ae4b68e6a0694f90
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-18 18:38:19 -0300

    Created naomi/__log__.py at Timestamp: 2019-02-18 21:38:18

---
+++ naomi/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Feb 18 2019 18:38:16.903 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from _spy.vitto
+                 ^
+SyntaxError: invalid syntax
+'''},
