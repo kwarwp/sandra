4ede28a8edcb1f6b70a975ad38eb83f577996e67
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-18 18:54:54 -0300

    Saved ruzwana/main.py at Timestamp: 2019-02-18 21:54:54

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,2 +1,3 @@
 # Thyago cestari
 from _spy.vittolino.main import Cena, Elemento, Texto
+Elemento_Diego ribas  ="https://pre00.deviantart.net/87a4/th/pre/f/2017/179/f/9/diego_ribas1_by_couqnydesigns-dbebgz5.png"
