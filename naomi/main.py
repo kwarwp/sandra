9a4d500a1fac52380445ed3172f8fde6c0b5251f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-18 18:59:53 -0300

    Saved naomi/main.py at Timestamp: 2019-02-18 21:59:52

--- naomi/main.py
+++ naomi/main.py
@@ -1,2 +1,3 @@
 # yasmin
 from _spy.vittolino.main import Cena, Elemento, Texto
+elemento_dog = "https://www.google.com.br/url?sa=i&source=images&cd=&ved=2ahUKEwje3vTWo8bgAhXKDrkGHROLBOgQjRx6BAgBEAU&url=http%3A%2F%2Fwww.tudodesenhos.com%2Fd%2Fcachorro-de-madame&psig=AOvVaw2zAdGAtFQUuNmNM8HylemR&ust=1550613172071540"
