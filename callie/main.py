d70bc6afa857d95d994490443dd33b28d3aee076
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-18 18:59:05 -0300

    Saved callie/main.py at Timestamp: 2019-02-18 21:59:05

--- callie/main.py
+++ callie/main.py
@@ -1,2 +1,3 @@
 # Pedro Henrique Barros
 from _spy,vittolino,main import Cena, Elemento, Texto
+Elemento_Night Wing="https://vignette.wikia.nocookie.net/nerdologiaheroespowers/images/2/23/Asa_Noturna.png/revision/latest?cb=20180206000240&path-prefix=pt-br"
