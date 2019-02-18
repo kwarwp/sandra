29515d2bd879bf539aadd16719942d3bdddeb299
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-18 18:59:10 -0300

    Saved courtney/main.py at Timestamp: 2019-02-18 21:59:10

--- courtney/main.py
+++ courtney/main.py
@@ -1,2 +1,3 @@
 # Manuela Pinheiro
 from _spy.vittolino.main import cena, Elemento, Texto
+elemento_minnie ="https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Minnie_Mouse.png/220px-Minnie_Mouse.png"
