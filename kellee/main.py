83c7093a7ff16e389214f9e76a9547298e70540e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-18 19:00:12 -0300

    Saved kellee/main.py at Timestamp: 2019-02-18 22:00:09

--- kellee/main.py
+++ kellee/main.py
@@ -1,2 +1,3 @@
 # ludimila rodrigues 
 from-spy.vitolino.main importi cena,elemento,texto
+elemento_miraculous ="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwijj7G4ocbgAhUsHbkGHUPBBp8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F395331673533068033%2F&psig=AOvVaw1CNXp8iYb7Pc_igM7955Vi&ust=1550612927951409"
