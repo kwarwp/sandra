bdbc8e9d492c5238d69b6d9887cb0a90b94951ad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-18 18:48:06 -0300

    Saved kathryn/main.py at Timestamp: 2019-02-18 21:48:05

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,2 +1,3 @@
 # Pedro Henrique
 from _spy.vittolino.main import cena, Elemento, Texto
+Elemento_deadpool="https://vignette.wikia.nocookie.net/xmenmovies/images/7/7c/Deadpool_%28Thumbs_Up_-_Transparent%29.png/revision/latest?cb=20170324222613"
