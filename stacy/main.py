c4085a4519c629fe0379bdbe6322d1b590d825da
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-18 18:57:17 -0300

    Saved stacy/main.py at Timestamp: 2019-02-18 21:57:17

--- stacy/main.py
+++ stacy/main.py
@@ -1,3 +1,3 @@
 # Caroline Maia Dias
 from _spy.vittalino.main import Cena,Elemento,Texto
-elemento_bobesponja = "https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiC2tDen8bgAhVxH7kGHWv-CgoQjRx6BAgBEAU&url=https%3A%2F%2Ffazendoanossafesta.com.br%2F2011%2F07%2Fimagens-e-molduras-do-bob-esponja-calca-quadrada.html%2F&psig=AOvVaw0TM5rlzVx2qvLij-VVIfym&ust=1550612435948773
+elemento_bobesponja = "https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiC2tDen8bgAhVxH7kGHWv-CgoQjRx6BAgBEAU&url=https%3A%2F%2Ffazendoanossafesta.com.br%2F2011%2F07%2Fimagens-e-molduras-do-bob-esponja-calca-quadrada.html%2F&psig=AOvVaw0TM5rlzVx2qvLij-VVIfym&ust=1550612435948773"
