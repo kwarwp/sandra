740d8a08c775540e91c8dc8db91d1ed915a23798
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-13 18:21:23 -0300

    Saved morgan/main.py at Timestamp: 2019-03-13 21:21:22

--- morgan/main.py
+++ morgan/main.py
@@ -1,3 +1,3 @@
 #davy azaredo
 from _spy.vittolino.main import Cena, Elemento, Texto
-elemento_gato = "https://sc.mogicons.com/l/garfield-172.png"
+elemento_ninja = "http://pluspng.com/img-png/ninja-hd-png-ninja-images-cnmuqi-hd-wallpapers-306.png"
