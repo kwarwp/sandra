6ad30b4d3036f1223cb1c8a3b3d65cbf779713b1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-13 18:53:21 -0300

    Saved sarah/main.py at Timestamp: 2019-03-13 21:53:20

--- sarah/main.py
+++ sarah/main.py
@@ -1,3 +1,8 @@
 # sara
 from _spy.vittolino.main import Cena, Elemento, Texto
-elemento_gato = "https://sc.mogicons.com/l/garfield-172.png"
+elemento_gato = "https://cdn3.iconfinder.com/data/icons/ice-cream-set-part-two/64/-ice-cream-34-512.png"
+ 
+def Historia():
+	cenahouse =cena (img = "" )
+    
+Historia()
