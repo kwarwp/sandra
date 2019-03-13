9d90db6a158c741ef13a18d2ae3c603b9bfb7830
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-13 18:37:18 -0300

    Created libby/__log__.py at Timestamp: 2019-03-13 21:37:17

---
+++ libby/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Mar 13 2019 18:37:13.414 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  def
+      ^
+SyntaxError: invalid syntax
+'''},
