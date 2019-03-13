4b7de7d5fbf5b5e1a137b0c007afc8c29e39bd1c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-13 18:50:43 -0300

    Saved tracy/main.py at Timestamp: 2019-03-13 21:50:43

--- tracy/main.py
+++ tracy/main.py
@@ -1,3 +1,8 @@
 # Cibele Ribeiro
 from _spy.vittolino.main import Cena, Elemento, Texto
 elemento_gato = "https://sc.mogicons.com/l/garfield-172.png"
+
+def Historia():
+	cenaHouse = Cena()
+
+Historia()
