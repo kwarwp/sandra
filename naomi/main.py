c68458d01b3a22bac21bd6fac678ba63ed5fa04f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-13 18:50:51 -0300

    Saved naomi/main.py at Timestamp: 2019-03-13 21:50:51

--- naomi/main.py
+++ naomi/main.py
@@ -1,3 +1,8 @@
 # yasmin
 from _spy.vittolino.main import Cena, Elemento, Texto
 elemento_ariana grande = "http://www.csanl.com.br/alunos/paginas/2017/f7b30/internet/png%203.png"
+
+def Historia():
+	cenaarianagrande = Cena()
+
+Historia()
