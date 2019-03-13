d1c38518383987b06b7d0a511bb0819356bd415c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-13 18:23:53 -0300

    Saved sarah/main.py at Timestamp: 2019-03-13 21:23:53

--- sarah/main.py
+++ sarah/main.py
@@ -1,3 +1,3 @@
-# sara
+# Cibele Ribeiro
 from _spy.vittolino.main import Cena, Elemento, Texto
-elemento_gato = "https://sc.mogicons.com/l/garfield-172.png"
+elemento_gato = "http://www.specialdog.com/snoopygarfield/wp-content/themes/snoopygarfield/images/garfield.png"
