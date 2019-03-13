a1d8ec5f3f3094386a6ad8fc547d1652390cf3f2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-13 18:50:45 -0300

    Saved libby/main.py at Timestamp: 2019-03-13 21:50:45

--- libby/main.py
+++ libby/main.py
@@ -1,3 +1,8 @@
 # gabriela ribeiro
 from _spy.vittolino.main import Cena, Elemento, Texto
 elemento_snoopy = "http://www.specialdog.com/snoopygarfield/wp-content/themes/snoopygarfield/images/snoopy.png"
+
+def Historia():
+	cenaQarto = Cena()
+
+Historia()
