21b89e4c2338b13a86391456aa92195309803e4b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-13 18:50:50 -0300

    Saved amanda/main.py at Timestamp: 2019-03-13 21:50:50

--- amanda/main.py
+++ amanda/main.py
@@ -1,3 +1,8 @@
 # Cléo Paiva
 from _spy.vittolino.main import Cena, Elemento, Texto
-elemento_peppa_pig = "https://sc.mogicons.com/l/garfield-172.png"
+elemento_peppa_pig = https://i.pinimg.com/originals/94/18/6b/94186bcd2fadec05417f6a90b5ba15fc.png
+
+def Historia():
+	cenaFloresta = Cena()
+
+Historia()
