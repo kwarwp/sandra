3c75a40197516985dc188b957671a2cd4c05034c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-13 18:50:52 -0300

    Saved sara/main.py at Timestamp: 2019-03-13 21:50:52

--- sara/main.py
+++ sara/main.py
@@ -1,2 +1,8 @@
 # ISABELA PEREIRA
 from _spy.vittolino.main import Cena, Elemento, Texto
+elemento-sou luna /https://encrypted-tbn0.gstatic.com/images?q=tbn:
+
+def Historia():
+	cenaShow = Cena()
+
+Historia()
