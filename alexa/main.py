c26bd6afe17ec3249cbda4317cbf625b7d07b9c7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-13 18:50:44 -0300

    Saved alexa/main.py at Timestamp: 2019-03-13 21:50:44

--- alexa/main.py
+++ alexa/main.py
@@ -1,3 +1,8 @@
-Jorge Fernando
+# Cibele Ribeiro
 from _spy.vittolino.main import Cena, Elemento, Texto
-elemento_bart = "https://sc.mogicons.com/l/garfield-172.png"
+elemento_gato = "https://sc.mogicons.com/l/garfield-172.png"
+
+def historia():
+	cenaHouse= Cena()
+    
+historia()
