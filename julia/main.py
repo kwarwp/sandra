4b252afdc1da2563db76295407d6cfafa5a8ba15
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:11:16 -0300

    Saved julia/main.py at Timestamp: 2019-03-25 21:11:15

--- julia/main.py
+++ julia/main.py
@@ -1,4 +1,4 @@
-# Cibele Ribeiro
+# Lana Soooooouza
 from _spy.vittolino.main import Cena, Elemento, Texto
 elemento_alien= https://cdn3.iconfinder.com/data/icons/pictofoundry-pro-vector-set/512/Alien-512.png""
 
