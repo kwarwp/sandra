9f061c0fb01411cd8a97339441613a65f607c40e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:11:34 -0300

    Created callie/__log__.py at Timestamp: 2019-03-25 21:11:33

---
+++ callie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Mar 25 2019 16:11:30.637 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from _spy,vittolino,main import Cena, Elemento, Texto
+           ^
+SyntaxError: invalid syntax
+'''},
