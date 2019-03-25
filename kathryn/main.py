e3109cf3d3d94e70cc0111461fd0e8c0c764f6d6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:45:58 -0300

    Saved kathryn/main.py at Timestamp: 2019-03-25 21:45:58

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,8 +1,8 @@
 # Pedro Henrique Baridoti Coelho
-from _spy.vittolino.main import cena, Elemento, Texto
+from _spy.vitollino.main import cena, Elemento, Texto
 Elemento_deadpool="https://vignette.wikia.nocookie.net/xmenmovies/images/7/7c/Deadpool_%28Thumbs_Up_-_Transparent%29.png/revision/latest?cb=20170324222613"
 
-ef Historia():
-	cenaHouse = Cena(img = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Wrestling%2C_Pavilh%C3%A3o_Atl%C3%A2ntico_1.jpg/250px-Wrestling%2C_Pavilh%C3%A3o_Atl%C3%A2ntico_1
+def Historia():
+	cenaHouse = Cena(img = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Wrestling%2C_Pavilh%C3%A3o_Atl%C3%A2ntico_1.jpg/250px-Wrestling%2C_Pavilh%C3%A3o_Atl%C3%A2ntico_1")
 	cenaHouse.vai()
 Historia()
