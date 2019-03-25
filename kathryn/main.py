c1075248ccaf30760a8b8d8f5991dd6f2a289144
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:50:56 -0300

    Saved kathryn/main.py at Timestamp: 2019-03-25 21:50:56

--- kathryn/main.py
+++ kathryn/main.py
@@ -3,6 +3,6 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 Elemento_deadpool="https://vignette.wikia.nocookie.net/xmenmovies/images/7/7c/Deadpool_%28Thumbs_Up_-_Transparent%29.png/revision/latest?cb=20170324222613"
 
 def Historia():
-	cenaHouse = Cena(img = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Wrestling%2C_Pavilh%C3%A3o_Atl%C3%A2ntico_1.jpg/250px-Wrestling%2C_Pavilh%C3%A3o_Atl%C3%A2ntico_1")
+	cenaHouse = Cena(img = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Wrestling%2C_Pavilh%C3%A3o_Atl%C3%A2ntico_1.jpg/250px-Wrestling%2C_Pavilh%C3%A3o_Atl%C3%A2ntico_1.jpg")
 	cenaHouse.vai()
 Historia()
