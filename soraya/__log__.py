bbf5bf3c7d4f15489dd1c5bcf13463b4f1994ca9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:43:09 -0300

    Created soraya/__log__.py at Timestamp: 2019-03-25 21:43:09

---
+++ soraya/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Mar 25 2019 18:43:06.609 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  cena House= Cena(mg ="http://jogosindie.com/wp-content/uploads/2014/08/lava.png")
+        ^
+SyntaxError: invalid syntax
+'''},
