01e1b9ed6b7baeadc3ad9171b080b31da0f7f5fe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:37:41 -0300

    Saved callie/main.py at Timestamp: 2019-03-25 21:37:40

--- callie/main.py
+++ callie/main.py
@@ -3,6 +3,7 @@ from _spy,vittolino,main import Cena, Elemento, Texto
 Elemento_Night Wing="https://vignette.wikia.nocookie.net/nerdologiaheroespowers/images/2/23/Asa_Noturna.png/revision/latest?cb=20180206000240&path-prefix=pt-br"def Historia():
 
 def Historia():
-	cenaHouse = Cena(img = "https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiuo7fonJ7hAhWFD7kGHfJ_ChYQjRx6BAgBEAU&url=http%3A%2F%2Fforums.daybreakgames.com%2Fdcuo%2Findex.php%3Fthreads%2Fnew-teen-titans-tower-look.296433%2F&psig=AOvVaw0VUZ1umKrPFlebIBx6AdDT&ust=1553635326971397")
-
+	cenatowertitan = Cena(img = "https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiuo7fonJ7hAhWFD7kGHfJ_ChYQjRx6BAgBEAU&url=http%3A%2F%2Fforums.daybreakgames.com%2Fdcuo%2Findex.php%3Fthreads%2Fnew-teen-titans-tower-look.296433%2F&psig=AOvVaw0VUZ1umKrPFlebIBx6AdDT&ust=1553635326971397")
+	cenatowertintanvai()
+    
 Historia()
