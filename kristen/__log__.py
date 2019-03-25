c2be8764abb321bffa88862c4d7b9f7c3771f343
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:48:49 -0300

    Saved kristen/__log__.py at Timestamp: 2019-03-25 21:48:49

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -20,3 +20,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Mar 25 2019 18:48:49.195 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  cenaHouse.vai()
+  ^
+IndentationError: unexpected indent
+'''},
