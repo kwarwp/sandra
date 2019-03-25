9921e3d21c37a75c6a09040d350c3877929149aa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:14:56 -0300

    Saved callie/main.py at Timestamp: 2019-03-25 21:14:56

--- callie/main.py
+++ callie/main.py
@@ -1,3 +1,7 @@
 # Pedro Henrique Barros
 from _spy,vittolino,main import Cena, Elemento, Texto
-Elemento_Night Wing="https://vignette.wikia.nocookie.net/nerdologiaheroespowers/images/2/23/Asa_Noturna.png/revision/latest?cb=20180206000240&path-prefix=pt-br"
+Elemento_Night Wing="https://vignette.wikia.nocookie.net/nerdologiaheroespowers/images/2/23/Asa_Noturna.png/revision/latest?cb=20180206000240&path-prefix=pt-br"def Historia():
+
+cenaHouse = Cena(img = "")
+
+Historia()
