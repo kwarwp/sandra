305b91d9615a76c4c551d1fc757ff24fbe630ca3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:38:15 -0300

    Saved tracy/main.py at Timestamp: 2019-03-25 21:38:15

--- tracy/main.py
+++ tracy/main.py
@@ -3,6 +3,6 @@ from _spy.vittolino.main import Cena, Elemento, Texto
 elemento_gato = "https://sc.mogicons.com/l/garfield-172.png"
 
 def Historia():
-	cenaHouse = Cena(img = "")
+	cenaHouse = Cena(img = "https://i.pinimg.com/originals/3f/36/7f/3f367f727042a6c6b03301167d113104.jpg")
 	cenaHouse.vai()
 Historia()
