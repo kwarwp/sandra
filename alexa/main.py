baea9ad96ccef15fba8364616f50bb2071e34687
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:43:26 -0300

    Saved alexa/main.py at Timestamp: 2019-03-25 21:43:26

--- alexa/main.py
+++ alexa/main.py
@@ -1,10 +1,9 @@
 # jorge
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 elemento_Bart = "https://sc.mogicons.com/l/Bart-172.png"
 
 def historia():
 	cenaHouse= Cena(img = "http://ossimpsons-orpg.weebly.com/uploads/1/2/9/6/12964529/850096305.jpg?554"    
-   
-	cenahouse.vai()
+	cenaHouse.vai()
    
 Historia()
