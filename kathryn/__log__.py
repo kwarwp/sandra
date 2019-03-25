939346a3abbd5980a1a49f4e4f05ba9a2f170fcd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:46:58 -0300

    Saved kathryn/__log__.py at Timestamp: 2019-03-25 21:46:58

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -6,3 +6,10 @@
                                                                                                                                                                                      ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Mon Mar 25 2019 18:46:59.701 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  Elemento_deadpool="https://vignette.wikia.nocookie.net/xmenmovies/images/7/7c/Deadpool_%28Thumbs_Up_-_Transparent%29.png/revision/latest?cb=20170324222613")
+                     ^
+SyntaxError: invalid syntax
+'''},
