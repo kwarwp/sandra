3bd23553bc6f6557acbeaaefd0cadc99f864de5a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:29:03 -0300

    Saved kristen/main.py at Timestamp: 2019-03-25 21:29:03

--- kristen/main.py
+++ kristen/main.py
@@ -1,8 +1,8 @@
-# sandra.kristen.main.py
+# Victor David 
 from _spy.vittolino.main import Cena, Elemento, Texto
-elemento_gato = "https://sc.mogicons.com/l/garfield-172.png"
+elemento_casalMouse = "https://www.google.com.br/ur"
 
 def Historia():
-	cenaHouse = Cena(img = "")
+	cenaHouse = Cena(img = "https://vignette.wikia.nocookie.net/simpsons/images/8/86/364.jpg/revision/latest/scale-to-width-down/400?cb=20110713113701&path-prefix=pt")
 
 Historia()
