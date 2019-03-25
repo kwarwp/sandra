77aff97a7cad56b97e28d8416b636141ed7db222
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:43:10 -0300

    Created kathryn/__log__.py at Timestamp: 2019-03-25 21:43:10

---
+++ kathryn/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Mar 25 2019 18:43:09.709 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  cenaHouse = Cena(img = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Wrestling%2C_Pavilh%C3%A3o_Atl%C3%A2ntico_1.jpg/250px-Wrestling%2C_Pavilh%C3%A3o_Atl%C3%A2ntico_1
+                                                                                                                                                                                     ^
+SyntaxError: EOL while scanning string literal
+'''},
