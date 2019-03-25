bd59206eb54ca436e65f6c0a2a2c829f79a27cde
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:41:31 -0300

    Created angie/__log__.py at Timestamp: 2019-03-25 21:41:31

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Mar 25 2019 18:41:30.904 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  elemento_casalMouse = "https://www.google.com.br/ur 
+                                                      ^
+SyntaxError: EOL while scanning string literal
+'''},
