85182e056f1add956973ac5c0baff67be4d1aab2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:49:53 -0300

    Saved kathryn/main.py at Timestamp: 2019-03-25 21:49:53

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,5 +1,5 @@
 # Pedro Henrique Baridoti Coelho
-from _spy.vitollino.main import cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 Elemento_deadpool="https://vignette.wikia.nocookie.net/xmenmovies/images/7/7c/Deadpool_%28Thumbs_Up_-_Transparent%29.png/revision/latest?cb=20170324222613"
 
 def Historia():
