4f34bd91160011db9fc2682f5998177d3a8c270a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:52:56 -0300

    Saved julia/__log__.py at Timestamp: 2019-03-25 21:52:56

--- julia/__log__.py
+++ julia/__log__.py
@@ -31,3 +31,21 @@ SyntaxError: invalid syntax
     	cenaFloresta.vai
 NameError: name 'cenaFloresta' is not defined
 '''},
+{'date': 'Mon Mar 25 2019 17:52:54.905 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    Historia()
+  module <module> line 7
+    	cenaFloresta.vai
+NameError: name 'cenaFloresta' is not defined
+'''},
