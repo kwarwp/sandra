ab085126813477a7996a0c584f6cfec74ee5d2e4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:52:17 -0300

    Saved parisa/__log__.py at Timestamp: 2019-03-25 21:52:17

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -85,3 +85,10 @@ SyntaxError: invalid syntax
         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Mar 25 2019 18:52:17.708 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  cena House = Cena(img = "https://pngimage.net/wp-content/uploads/2018/05/cen%C3%A1rio-png.png")
+        ^
+SyntaxError: invalid syntax
+'''},
