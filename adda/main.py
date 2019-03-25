41903fc43997862f35bad37229ff51485f1a8e49
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:42:39 -0300

    Saved adda/main.py at Timestamp: 2019-03-25 21:42:38

--- adda/main.py
+++ adda/main.py
@@ -4,7 +4,7 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 elemento_gato = "http://mtv.mtvnimages.com/apps/series-art/teen-wolf/teenwolf_web_series_hero_1920x540_013019.jpg?quality=0.85&width=959&height=540&crop=true"
 
 def Historia():
-	cenaHouse = Cena(img = "")
+	cenaHouse = Cena(img = "https://vignette.wikia.nocookie.net/teenwolfserie/images/1/14/Teen_Wolf_Season_3_Episode_1_Beacon_Hills_Street_Night.png/revision/latest?cb=20180411122138&path-prefix=pt-br")
 	cenaHouse.vai()
 Historia()
 
