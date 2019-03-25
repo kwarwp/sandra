3d8ce50b198ecb9692517eaac36dce992f01955d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:40:46 -0300

    Saved alexa/main.py at Timestamp: 2019-03-25 21:40:45

--- alexa/main.py
+++ alexa/main.py
@@ -5,6 +5,6 @@ elemento_Bart = "https://sc.mogicons.com/l/Bart-172.png"
 def historia():
 	cenaHouse= Cena(img = "http://ossimpsons-orpg.weebly.com/uploads/1/2/9/6/12964529/850096305.jpg?554"    
    
-	cenahouse.vai
+	cenahouse.vai()
    
 Historia()
