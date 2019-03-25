9992e1f4b5688bf09ff949e5deb227bdf81501e6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:48:58 -0300

    Saved kathryn/__log__.py at Timestamp: 2019-03-25 21:48:58

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -13,3 +13,34 @@ SyntaxError: EOL while scanning string literal
                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Mar 25 2019 18:48:58.836 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from _spy.vitollino.main import cena, Elemento, Texto
+ImportError: cannot import name 'cena'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from _spy.vitollino.main import cena, Elemento, Texto
+'''},
