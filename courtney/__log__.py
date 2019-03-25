d9a5ee05867e4751e1df54ff7b93c3eb27ae390e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:54:06 -0300

    Created courtney/__log__.py at Timestamp: 2019-03-25 21:54:06

---
+++ courtney/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Mar 25 2019 18:54:05.307 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  cena House = Cena(img = "https://pngimage.net/wp-content/uploads/2018/05/cen%C3%A1rio-png.png")
+        ^
+SyntaxError: invalid syntax
+'''},
