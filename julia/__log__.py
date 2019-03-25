21a217dad2040ab9c1e998979a6be11698985ddc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:41:16 -0300

    Saved julia/__log__.py at Timestamp: 2019-03-25 21:41:15

--- julia/__log__.py
+++ julia/__log__.py
@@ -6,3 +6,10 @@
                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Mar 25 2019 17:41:15.605 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  elemento_alien= https://cdn3.iconfinder.com/data/icons/pictofoundry-pro-vector-set/512/Alien-512.png""
+                        ^
+SyntaxError: invalid syntax
+'''},
