73b713335216dacb1dec473c7ff585e7c4469830
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:41:13 -0300

    Created kristen/__log__.py at Timestamp: 2019-03-25 21:41:13

---
+++ kristen/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Mar 25 2019 17:41:12.8 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  cenaHouse.vai()
+  ^
+IndentationError: unexpected indent
+'''},
