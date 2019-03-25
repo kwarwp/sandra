6223eaa47f7a9f4de8f44d586fa73d10e7ef83d6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:22:36 -0300

    Saved parisa/main.py at Timestamp: 2019-03-25 21:22:36

--- parisa/main.py
+++ parisa/main.py
@@ -3,6 +3,6 @@ from _spy.vittolino.main import Cena, Elemento,Texto
 elemento_mickey ="https://blogs.acu.edu/scholarslab/files/2018/12/MickeyMouse.png"
 
 def Historia():
-	cena House= Cena(img ="https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiLia7dm57hAhWeKLkGHULnC1EQjRx6BAgBEAU&url=https%3A%2F%2Fpngimage.net%2Fcenario-png%2F&psig=AOvVaw02urD0m-0zxSQRFlKFSpP-&ust=1553635040733791")
+	cena House= Cena(img ="https://pngimage.net/wp-content/uploads/2018/05/cen%C3%A1rio-png.png")
     
 Historia ()
