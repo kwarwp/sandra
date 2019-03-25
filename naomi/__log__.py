812b66685cfbff3d5738f87c28b7a5a486cdc803
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:44:17 -0300

    Saved naomi/__log__.py at Timestamp: 2019-03-25 21:44:17

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -6,3 +6,10 @@
                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Mar 25 2019 16:44:15.625 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  elemento_ariana grande = "https://img2.gratispng.com/20180706/zgy/kisspng-ariana-grande-moonlight-dangerous-woman-drawing-ariana-grande-cartoon-5b3f1f6e04fb88.6174859915308634700204.jpgv"
+                   ^
+SyntaxError: invalid syntax
+'''},
