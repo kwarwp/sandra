a759117c85e836b2e0b41789e232121739bc3b01
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:43:29 -0300

    Saved alexa/__log__.py at Timestamp: 2019-03-25 21:43:29

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: invalid syntax
                                                                                                              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Mar 25 2019 17:43:28.444 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  cenaHouse= Cena(img = "http://ossimpsons-orpg.weebly.com/uploads/1/2/9/6/12964529/850096305.jpg?554"    
+                                                                                                             ^
+SyntaxError: invalid syntax
+'''},
