fdbc945f8f7ff6340b7eda40b4a9bb650334ac07
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:44:48 -0300

    Saved soraya/main.py at Timestamp: 2019-03-25 21:44:48

--- soraya/main.py
+++ soraya/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 elemento_gato = "https://sc.mogicons.com/l/garfield-172.png"
 
 def Historia():
-	cenaHouse= Cena(mg ="http://jogosindie.com/wp-content/uploads/2014/08/lava.png")
+	cenaHouse= Cena(img ="http://jogosindie.com/wp-content/uploads/2014/08/lava.png")
 	cenaHouse.vai()
-História()
+Historia()
     
