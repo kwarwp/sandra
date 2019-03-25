f08d1d62dbb65878d573bed147060562c09f341d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:22:13 -0300

    Saved libby/main.py at Timestamp: 2019-03-25 21:22:12

--- libby/main.py
+++ libby/main.py
@@ -3,6 +3,6 @@ from _spy.vittolino.main import Cena, Elemento, Texto
 elemento_snoopy = "http://www.specialdog.com/snoopygarfield/wp-content/themes/snoopygarfield/images/snoopy.png"
 
 def Historia():
-	cenaQarto = Cena()
+	cenaQarto = Cena(img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCx-MkZzcdk8uwwRpah7bUzH65QoU0dIxen-7bJp9COzSLi1CR")
 
 Historia()
