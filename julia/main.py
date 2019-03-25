655289bdf3c3337cceb5f58dc1ec7ddab77ff155
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:50:53 -0300

    Saved julia/main.py at Timestamp: 2019-03-25 21:50:53

--- julia/main.py
+++ julia/main.py
@@ -1,5 +1,5 @@
 # Lana Soooooouza
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 elemento_alien= https://cdn3.iconfinder.com/data/icons/pictofoundry-pro-vector-set/512/Alien-512.png""
 
 def Historia():
