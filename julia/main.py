eedd6c5e0de77c69760cbdb28f1a4cf1ad3a3773
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:51:47 -0300

    Saved julia/main.py at Timestamp: 2019-03-25 21:51:47

--- julia/main.py
+++ julia/main.py
@@ -1,6 +1,6 @@
 # Lana Soooooouza
 from _spy.vitollino.main import Cena, Elemento, Texto
-elemento_alien= https://cdn3.iconfinder.com/data/icons/pictofoundry-pro-vector-set/512/Alien-512.png""
+elemento_alien = "https://cdn3.iconfinder.com/data/icons/pictofoundry-pro-vector-set/512/Alien-512.png"
 
 def Historia():
 	cenaHouse = Cena(img = "https://img.elo7.com.br/product/zoom/1360BB4/painel-floresta-g-frete-gratis-fundo-festa-infantil.jpg")
