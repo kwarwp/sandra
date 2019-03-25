f79699d4ede4ff40f2eab1ebd6b1e2892b81fe1a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:52:26 -0300

    Saved angie/main.py at Timestamp: 2019-03-25 21:52:25

--- angie/main.py
+++ angie/main.py
@@ -3,7 +3,6 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 elemento_casalMouse = "https://akns-images.eonline.com/eol_images/Entire_Site/20141018/rs_560x415-141118090112-1024-mickey-minnie-mouse-kiss.ls.111814.jpg?fit=inside|900:auto&output-quality=90" 
 
 def Historia():
-cenaHouse = Cena(img = "https://img.elo7.com.br/product/zoom/D70D91/painel-casa-do-mickey-mouse-tema-de-festa-infantil.jpg"
-    
-      cenaHouse.vai()
+	cenaHouse = Cena(img = "https://img.elo7.com.br/product/zoom/D70D91/painel-casa-do-mickey-mouse-tema-de-festa-infantil.jpg"
+	cenaHouse.vai()   
 Historia()
