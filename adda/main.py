002d883b0e4d000c4946427e07995672863a6e09
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:15:08 -0300

    Created adda/main.py at Timestamp: 2019-03-25 21:15:08

---
+++ adda/main.py
@@ -0,0 +1,10 @@
+# pedro henrique cordeiro
+
+from _spy.vittolino.main import Cena, Elemento, Texto
+elemento_gato = "https://sc.mogicons.com/l/garfield-172.png"
+
+def Historia():
+	cenaHouse = Cena(img = "https://i.pinimg.com/originals/3f/36/7f/3f367f727042a6c6b03301167d113104.jpg")
+
+Historia()
+
