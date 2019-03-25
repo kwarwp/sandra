c8a06364e2e915c78ede79b97eb6cd5ba09e8107
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:48:32 -0300

    Saved angie/main.py at Timestamp: 2019-03-25 21:48:32

--- angie/main.py
+++ angie/main.py
@@ -1,5 +1,5 @@
 # Maria Luiza Sarzedas
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 elemento_casalMouse = "https://www.google.com.br/ur 
 
 def Historia():
