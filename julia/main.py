69bb26945f6421bf7a6319a669e489e9a54b9dd3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:31:14 -0300

    Saved julia/main.py at Timestamp: 2019-03-25 21:31:14

--- julia/main.py
+++ julia/main.py
@@ -3,6 +3,6 @@ from _spy.vittolino.main import Cena, Elemento, Texto
 elemento_alien= https://cdn3.iconfinder.com/data/icons/pictofoundry-pro-vector-set/512/Alien-512.png""
 
 def Historia():
-
+	cenaHouse = Cena(img = "")
 
 Historia()
