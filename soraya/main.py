5476456cc91f983f50c79ea9b727b0cf1b7b2305
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:44:21 -0300

    Saved soraya/main.py at Timestamp: 2019-03-25 21:44:21

--- soraya/main.py
+++ soraya/main.py
@@ -1,9 +1,9 @@
 # Larissa dos Anjos
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 elemento_gato = "https://sc.mogicons.com/l/garfield-172.png"
 
 def Historia():
-	cena House= Cena(img ="http://jogosindie.com/wp-content/uploads/2014/08/lava.png")
+	cenaHouse= Cena(mg ="http://jogosindie.com/wp-content/uploads/2014/08/lava.png")
 	cenaHouse.vai()
 História()
     
