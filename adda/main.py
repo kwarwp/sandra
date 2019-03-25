b5588f0893ff581349fc4b19ffe290143f968add
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:41:52 -0300

    Saved adda/main.py at Timestamp: 2019-03-25 21:41:52

--- adda/main.py
+++ adda/main.py
@@ -1,6 +1,6 @@
 # pedro henrique cordeiro
 
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 elemento_gato = "http://mtv.mtvnimages.com/apps/series-art/teen-wolf/teenwolf_web_series_hero_1920x540_013019.jpg?quality=0.85&width=959&height=540&crop=true"
 
 def Historia():
