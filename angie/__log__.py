7a7955496d57ce8db02e81d6d856203975d76f75
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:52:53 -0300

    Saved angie/__log__.py at Timestamp: 2019-03-25 21:52:53

--- angie/__log__.py
+++ angie/__log__.py
@@ -27,3 +27,10 @@ SyntaxError: EOL while scanning string literal
                                                       ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Mon Mar 25 2019 18:52:55.727 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  cenaHouse = Cena(img = "https://img.elo7.com.br/product/zoom/D70D91/painel-casa-do-mickey-mouse-tema-de-festa-infantil.jpg"
+                                                                                                                                ^
+SyntaxError: invalid syntax
+'''},
