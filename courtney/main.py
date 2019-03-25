016f3fad507634fa68b194586ea731428a62f5e7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:23:53 -0300

    Saved courtney/main.py at Timestamp: 2019-03-25 21:23:53

--- courtney/main.py
+++ courtney/main.py
@@ -1,3 +1,8 @@
 # Manuela Pinheiro
 from _spy.vittolino.main import cena, Elemento, Texto
 elemento_minnie ="https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Minnie_Mouse.png/220px-Minnie_Mouse.png"
+
+def Historia():
+	cena House= Cena(img ="https://pngimage.net/wp-content/uploads/2018/05/cen%C3%A1rio-png.png")
+    
+Historia ()
