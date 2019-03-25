9adce407abef429143ee46144811db5c218ec8fa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:43:51 -0300

    Saved angie/__log__.py at Timestamp: 2019-03-25 21:43:51

--- angie/__log__.py
+++ angie/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: EOL while scanning string literal
                                                       ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Mon Mar 25 2019 18:43:53.232 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  elemento_casalMouse = "https://www.google.com.br/ur 
+                                                      ^
+SyntaxError: EOL while scanning string literal
+'''},
