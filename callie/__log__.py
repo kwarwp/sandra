3779bb3144cc3b356d649db460e47ed4634e2e49
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-25 18:41:14 -0300

    Saved callie/__log__.py at Timestamp: 2019-03-25 21:41:13

--- callie/__log__.py
+++ callie/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Mar 25 2019 16:41:13.188 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from _spy,vittolino,main import Cena, Elemento, Texto
+           ^
+SyntaxError: invalid syntax
+'''},
