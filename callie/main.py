fd511549752da646bcf5a808ce2ed706d56ab124
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-29 19:02:31 -0300

    Saved callie/main.py at Timestamp: 2019-03-29 22:02:31

--- callie/main.py
+++ callie/main.py
@@ -4,5 +4,6 @@ Elemento_Night Wing="https://vignette.wikia.nocookie.net/nerdologiaheroespowers/
 
 def Historia():
 	cenatowertitan = Cena(img = "https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiuo7fonJ7hAhWFD7kGHfJ_ChYQjRx6BAgBEAU&url=http%3A%2F%2Fforums.daybreakgames.com%2Fdcuo%2Findex.php%3Fthreads%2Fnew-teen-titans-tower-look.296433%2F&psig=AOvVaw0VUZ1umKrPFlebIBx6AdDT&ust=1553635326971397")
-	cenatowertitan.vai()   
+	cenatowertitan.vai()
+    
 Historia()
