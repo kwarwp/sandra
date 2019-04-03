7dd8ee9615c372ec4040db64e4f015e47c389e72
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:54:28 -0300

    Saved julia/__log__.py at Timestamp: 2019-04-03 21:54:28

--- julia/__log__.py
+++ julia/__log__.py
@@ -85,3 +85,21 @@ NameError: name 'cenaFloresta' is not defined
     	cenaFloresta.vai
 NameError: name 'cenaFloresta' is not defined
 '''},
+{'date': 'Wed Apr 03 2019 18:54:27.990 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    Historia() 
+  module <module> line 11
+    	cenaFloresta.vai
+NameError: name 'cenaFloresta' is not defined
+'''},
