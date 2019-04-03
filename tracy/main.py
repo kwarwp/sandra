45f710d807fbb26dda277e1389912d3dc6284583
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:36:13 -0300

    Saved tracy/main.py at Timestamp: 2019-04-03 21:36:12

--- tracy/main.py
+++ tracy/main.py
@@ -4,5 +4,8 @@ elemento_gato = "https://sc.mogicons.com/l/garfield-172.png"
 
 def Historia():
 	cenaHouse = Cena(img = "https://i.pinimg.com/originals/3f/36/7f/3f367f727042a6c6b03301167d113104.jpg")
+	gato = Elemento (img = elemento_gato,
+                      tit="Gato Laranja",
+                      style = dict( left=150, top =60, width=60, height=200))
 	cenaHouse.vai()
 Historia()
