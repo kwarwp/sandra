2a6fd8282da2f7c8cb8d2c1e74561fd323c8c1a3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:46:47 -0300

    Saved libby/__log__.py at Timestamp: 2019-04-03 21:46:47

--- libby/__log__.py
+++ libby/__log__.py
@@ -42,3 +42,21 @@ TypeError: 'module' object is not callable
     	cenaQuarto = Cena(img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCx-MkZzcdk8uwwRpah7bUzH65QoU0dIxen-7bJp9COzSLi1CR")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Wed Apr 03 2019 18:46:47.393 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    Historia()
+  module <module> line 7
+    	snoopy = Elemento (img = Elemento_cachorro,
+NameError: name 'Elemento_cachorro' is not defined
+'''},
