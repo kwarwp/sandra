df64383e89c23b9f09f51855d45f82bf62d4fa1b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:52:50 -0300

    Created ruzwana/__log__.py at Timestamp: 2019-04-03 21:52:50

---
+++ ruzwana/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Apr 03 2019 18:52:48.328 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  Elemento_Diego ribas  ="https://pre00.deviantart.net/87a4/th/pre/f/2017/179/f/9/diego_ribas1_by_couqnydesigns-dbebgz5.png"
+                  ^
+SyntaxError: invalid syntax
+'''},
