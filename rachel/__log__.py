eca9d9005f26cda1f23f0d8665d0f5cd7a7bf2d6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:51:17 -0300

    Saved rachel/__log__.py at Timestamp: 2019-04-03 21:51:17

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: EOL while scanning string literal
                                                                                          ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Wed Apr 03 2019 18:51:16.834 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  elemento_kaneki = "https://data.whicdn.com/images/204362721/superthumb.png?t=1445214745
+                                                                                         ^
+SyntaxError: EOL while scanning string literal
+'''},
