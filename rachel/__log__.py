ce22d9aa589acaee8dafd3b986d1c504aa23aba2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:46:46 -0300

    Created rachel/__log__.py at Timestamp: 2019-04-03 21:46:46

---
+++ rachel/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Apr 03 2019 18:46:44.108 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  elemento_kaneki = "https://data.whicdn.com/images/204362721/superthumb.png?t=1445214745
+                                                                                         ^
+SyntaxError: EOL while scanning string literal
+'''},
