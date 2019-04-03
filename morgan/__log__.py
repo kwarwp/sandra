56ab5605b72fff9c1c2103af4ad833ab4f35027c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:11:01 -0300

    Created morgan/__log__.py at Timestamp: 2019-04-03 21:11:00

---
+++ morgan/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Wed Apr 03 2019 18:10:58.300 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 9
+    historia()
+  module <module> line 6
+    	cenaNinja = Cena(img = "https://i1.wp.com/homensdacasa.net/wp-content/uploads/2017/08/leitor-ninja-casa-lara-84.jpg?resize=960%2C642")
+TypeError: 'module' object is not callable
+'''},
