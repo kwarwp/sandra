bb4bd35fd7f284dc992be9124ad80c42d7cd1ed4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:31:15 -0300

    Saved julia/main.py at Timestamp: 2019-04-03 21:31:14

--- julia/main.py
+++ julia/main.py
@@ -5,6 +5,6 @@ elemento_alien = "https://cdn3.iconfinder.com/data/icons/pictofoundry-pro-vector
 def Historia():
 	cenaHouse = Cena(img = "https://img.elo7.com.br/product/zoom/1360BB4/painel-floresta-g-frete-gratis-fundo-festa-infantil.jpg")
 	alien = Elemento ( img = elemento_alien,
-                       tit="Gato 
+                       tit="Alien Laranja",
 	cenaFloresta.vai
 Historia() 
