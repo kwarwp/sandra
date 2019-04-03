2a686876e06a2e8d44136434a98fa6d77a8068dd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:51:45 -0300

    Saved courtney/main.py at Timestamp: 2019-04-03 21:51:44

--- courtney/main.py
+++ courtney/main.py
@@ -2,11 +2,11 @@
 from _spy.vittolino.main import cena, Elemento, Texto
 elemento_minnie ="https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Minnie_Mouse.png/220px-Minnie_Mouse.png"
 
-def Historia():=
+def Historia():
 	cena House = Cena(img = "https://pngimage.net/wp-content/uploads/2018/05/cen%C3%A1rio-png.png")
     minnie = Elemento(img = elemento_gato,
                      tit ='Gato Laranja',
-                     style = dict ( let=150,top=60,width 60 heigh=200)
+                     style = dict ( let=150,top=60,width 60, heigh=200))
 	minnie,entra(cenaHouse)
     cenaHouse.vai()
 Historia ()
