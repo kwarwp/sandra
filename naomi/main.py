e8e9213ea84e29034f404b810c8eb370199c5cce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:56:48 -0300

    Saved naomi/main.py at Timestamp: 2019-04-03 21:56:48

--- naomi/main.py
+++ naomi/main.py
@@ -3,8 +3,11 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 elemento_ariana_grande = "https://img2.gratispng.com/20180706/zgy/kisspng-ariana-grande-moonlight-dangerous-woman-drawing-ariana-grande-cartoon-5b3f1f6e04fb88.6174859915308634700204.jpgv"
 
 def Historia():
-	cenaarianagrande = Cena( img ="http://3.bp.blogspot.com/-flrppDI3vno/VRx579SLLKI/AAAAAAAABlc/6GtG4uTfYwY/s1600/cerejeiras%2Bjap%C3%A3o.jpg")  
-	cenaarianagrande.vai()
-Historia()    
+	cenaarianagrande = Cena( img ="http://3.bp.blogspot.com/-flrppDI3vno/VRx579SLLKI/AAAAAAAABlc/6GtG4uTfYwY/s1600/cerejeiras%2Bjap%C3%A3o.jp
+	Arianagrande=Elemento(img=elemento_Arianagrande,
+                     tit=''Arianagrande'',
+                            style=dict(left=150, top=60,width=60,height=200
+	arianagrande                            
+    
              
         
