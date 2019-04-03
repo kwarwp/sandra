48d7c3cc3d56b88a1aab8eed49854acf043cf520
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:49:23 -0300

    Saved parisa/main.py at Timestamp: 2019-04-03 21:49:22

--- parisa/main.py
+++ parisa/main.py
@@ -5,7 +5,7 @@ elemento_mickey ="https://blogs.acu.edu/scholarslab/files/2018/12/MickeyMouse.pn
 def Historia():
 	cenaHouse = Cena(img = "https://pngimage.net/wp-content/uploads/2018/05/cen%C3%A1rio-png.png")
 	mickey = Elemento (img = elemento_mickey,
-                        tit="Gato Laranja",
+                        tit="Mickey Laranja",
                         style = dict( left=150, top =60 width 60 heigh =200)
 	mickey,entra(cenaHouse)
 	cenaHouse.vai()
