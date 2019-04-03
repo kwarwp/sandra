8222b89cd444de104abdf4f01119c078d854cdf5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:48:11 -0300

    Saved libby/main.py at Timestamp: 2019-04-03 21:48:11

--- libby/main.py
+++ libby/main.py
@@ -4,9 +4,9 @@ elemento_snoopy = "http://www.specialdog.com/snoopygarfield/wp-content/themes/sn
 
 def Historia():
 	cenaQuarto = Cena(img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCx-MkZzcdk8uwwRpah7bUzH65QoU0dIxen-7bJp9COzSLi1CR")
-	snoopy = Elemento (img = Elemento_cachorro,
-                        tit="cachorro branco",
+	snoopy = Elemento (img = Elemento_snoopy,
+                        tit="snoopy branco",
                         style = dict( left=150, top=60, width=60,height=200))
-	cachorro.entra(cenaQuarto)
+	snoopy.entra(cenaQuarto)
 	cenaQuarto.vai()    
 Historia()
