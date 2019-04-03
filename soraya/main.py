3351da75e797833e18929635923f49fb62ffd2d8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:49:03 -0300

    Saved soraya/main.py at Timestamp: 2019-04-03 21:49:02

--- soraya/main.py
+++ soraya/main.py
@@ -4,6 +4,10 @@ elemento_gato = "https://sc.mogicons.com/l/garfield-172.png"
 
 def Historia():
 	cenaHouse= Cena(img ="http://jogosindie.com/wp-content/uploads/2014/08/lava.png")
+	gato = Elemento (img = elemento_gato,
+                      tit="Gato Laranja",
+                      style = dict( left=150, top =60, width=60, height=200))
+	gato.entra(cenaHouse)
 	cenaHouse.vai()
 Historia()
     
