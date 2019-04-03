cd2a1e8eefd98c8dd3d9cfd1c64d3f08e48cfb42
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:55:36 -0300

    Saved morgan/__log__.py at Timestamp: 2019-04-03 21:55:36

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -59,3 +59,10 @@ SyntaxError: invalid syntax
                                                                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Apr 03 2019 18:55:36.204 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  ninja = Elemento (img=elemento_ninja, 
+                                                                                                                                                                   ^
+SyntaxError: invalid syntax
+'''},
