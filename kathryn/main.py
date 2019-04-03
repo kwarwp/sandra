9fc7077ad7488a990ece0e400a3eb4db07ecd4b1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:53:08 -0300

    Saved kathryn/main.py at Timestamp: 2019-04-03 21:53:08

--- kathryn/main.py
+++ kathryn/main.py
@@ -4,9 +4,9 @@ Elemento_deadpool="https://vignette.wikia.nocookie.net/xmenmovies/images/7/7c/De
 
 def Historia():
 	cenaHouse = Cena(img = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Wrestling%2C_Pavilh%C3%A3o_Atl%C3%A2ntico_1.jpg/250px-Wrestling%2C_Pavilh%C3%A3o_Atl%C3%A2ntico_1.jpg")
-	Deadpoo = Elemento(img=elemento_Deadpoo,
-                        tit=Deadpoo",
-                        style = dict(left=150,top =60,width=60,hright=200
-	Deadpooo,entra(cenaHouse)
+	Deadpoo = Elemento(img=Elemento_deadpool,
+                        tit="Deadpoo",
+                        style = dict(left=150,top =60,width=60,height=200))
+	Deadpoo.entra(cenaHouse)
 	cenaHouse.vai()
 Hitoria()
