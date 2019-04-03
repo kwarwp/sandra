b4ddc10e2e85df6ff1af09e89346f9efc55bfd8e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:56:38 -0300

    Saved morgan/__log__.py at Timestamp: 2019-04-03 21:56:37

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -162,3 +162,19 @@ NameError: name 'Historia' is not defined
     Historia()
 NameError: name 'Historia' is not defined
 '''},
+{'date': 'Wed Apr 03 2019 18:56:33.948 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    Historia()
+NameError: name 'Historia' is not defined
+'''},
