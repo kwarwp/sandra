15f3899ad69d5b1591b863217b50432ce725190c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-03 18:38:52 -0300

    Saved morgan/main.py at Timestamp: 2019-04-03 21:38:52

--- morgan/main.py
+++ morgan/main.py
@@ -3,9 +3,9 @@ from _spy.vittolino.main import Cena, Elemento, Texto
 elemento_ninja = "http://pluspng.com/img-png/ninja-hd-png-ninja-images-cnmuqi-hd-wallpapers-306.png"
 
 def historia():
-	cenaNinja = Cena(img = "")
+	 cenaNinja = Cena(img = ""
 	ninja = Elemento (img = elemento_ninja, 
-                tit="Ninja preto",
-	  cenaNinja.vai()
-    
+                       tit="ninja preto
+                       style = dict( left=150, top =60, width=60, height=200))
+	cenaNinja.vai()
 historia()
