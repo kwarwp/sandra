dd4e1da3f27e7bc1b0974c7ba173a2e504a93058
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-04 15:23:01 -0300

    Created danae/main.py at Timestamp: 2019-04-04 18:23:01

---
+++ danae/main.py
@@ -0,0 +1,9 @@
+from _spy.vittolino.main import Cena, Elemento, Texto
+
+# link do castelo: https://img.elo7.com.br/product/original/1AD3471/painel-1x6
+linkDoShrek = "https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/shrek-swamp-perplexed.jpg"
+
+def Historia():
+  # HI I AM JUREG AND I LIKE TO EAT MOSQUITOS
+  # HI I AM JEFFERSON AND I LIKE STROGONOFF
+
