a5c36f72dbbac64bd1699780e859e9c56fb7f6d0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-04 15:10:40 -0300

    Saved danae/__log__.py at Timestamp: 2019-04-04 18:10:39

--- danae/__log__.py
+++ danae/__log__.py
@@ -27,3 +27,10 @@ SyntaxError: invalid syntax
                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Apr 04 2019 15:10:38.196 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  
+  ^
+IndentationError: expected an indented block
+'''},
