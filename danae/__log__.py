d2723f21bd6e6d699f14613d597250c926877e50
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-04 14:51:49 -0300

    Saved danae/__log__.py at Timestamp: 2019-04-04 17:51:49

--- danae/__log__.py
+++ danae/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Apr 04 2019 14:51:48.83 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  = link do castelo: https://img.elo7.com.br/product/original/1AD3471/painel-1x6
+   ^
+SyntaxError: invalid syntax
+'''},
