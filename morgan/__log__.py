5e374f74fe84558b5fbde482ffb45107c493c81a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 18:18:43 -0300

    Saved morgan/__log__.py at Timestamp: 2019-04-17 21:18:42

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -336,3 +336,21 @@ NameError: name 'cenaHouse' is not defined
     Historia()
 NameError: name 'Historia' is not defined
 '''},
+{'date': 'Wed Apr 17 2019 18:18:41.924 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    Historia()
+  module <module> line 6
+    	cenaHouse= Cena(img = "http://farm3.static.flickr.com/2290/1888495823_ef459b5c3e.jpg")
+TypeError: 'module' object is not callable
+'''},
