b1fbbecb3ba8bcde0a621391f8e2d1293fa4198c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 18:20:48 -0300

    Saved alexa/main.py at Timestamp: 2019-04-17 21:20:48

--- alexa/main.py
+++ alexa/main.py
@@ -5,8 +5,8 @@ elemento_Bart = "https://sc.mogicons.com/l/Bart-172.png"
 def Historia():
 	cenaHouse= Cena(img = "http://ossimpsons-orpg.weebly.com/uploads/1/2/9/6/12964529/850096305.jpg?554")    
 	bart = Elemento (img = elemento_Bart,
-                  tit="Bart amarelo",
-                  style = dict(left 150,top=60,width=60, height=200))
+                      tit="Bart amarelo",
+                      style = dict(left=150,top=60,width=60, height=200))
 	bart.entra(cenaHouse) 
 	txtBart = Texto( cenaHouse,
                  "Estou com fome!")
