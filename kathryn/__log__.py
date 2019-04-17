38d8a506a79a174ea72fb09e7264ce04c4f893eb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 18:30:53 -0300

    Saved kathryn/__log__.py at Timestamp: 2019-04-17 21:30:53

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -85,3 +85,21 @@ NameError: name 'Hitoria' is not defined
     	Deadpoo = txtdeadpoo.vai
 NameError: name 'txtdeadpoo' is not defined
 '''},
+{'date': 'Wed Apr 17 2019 17:30:52.5 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    Historia()
+  module <module> line 13
+    	Deadpoo = txtdeadpoo.vai
+NameError: name 'txtdeadpoo' is not defined
+'''},
