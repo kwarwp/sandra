f5739527433410a52844bac66b38e7d1ffd6bc2e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 18:31:12 -0300

    Saved rachel/main.py at Timestamp: 2019-04-17 21:31:12

--- rachel/main.py
+++ rachel/main.py
@@ -1,12 +1,18 @@
 #danielmartins
 from _spy.vittolino.main import Cena, Elemento, Texto
 elemento_kaneki = "https://data.whicdn.com/images/204362721/superthumb.png?t=1445214745
-
-def historia():
-	cenakaneki=cena()
+  
+def Historia():
+	cenaKaneki= Cena()
 	kaneki=Elemento(img=elemento_kaneki,
-                      tit="kaneki preto",
-                      sryle=dict( left=150, top =60, height=200))                    
-	kaneki.entra(cenakaneki)
-	cenakaneki.vai()    
+tit="kaneki preto",
+                      style=dict( left=150, top =60, width=60, height=200)) 
+	kaneki.entra(cenaKaneki)                                    
+	txtKanrki=Texto(cenaKaneki,
+                     "Eleta com fome "
+
+	  
+      kaneki.vai=txtkaneki.vai
+	cenakaneki.vai()  
+    
 Historia()  
