345b2f066930d32ff7b11bd3a65934fe19c78c92
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 18:25:09 -0300

    Saved callie/main.py at Timestamp: 2019-04-17 21:25:09

--- callie/main.py
+++ callie/main.py
@@ -1,14 +1,14 @@
 # Pedro Henrique Barros	
 from _spy.vitollino.main import Cena, Elemento, Texto
-elemento_Night_Wing="https://vignette.wikia.nocookie.net/nerdologiaheroespowers/images/2/23/Asa_Noturna.png/revision/latest?cb=20180206000240&path-prefix=pt-br"
+elemento_night_wing="https://vignette.wikia.nocookie.net/nerdologiaheroespowers/images/2/23/Asa_Noturna.png/revision/latest?cb=20180206000240&path-prefix=pt-br"
 def Historia():
-	cenatowertitan = Cena(img = "https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiuo7fonJ7hAhWFD7kGHfJ_ChYQjRx6BAgBEAU&url=http%3A%2F%2Fforums.daybreakgames.com%2Fdcuo%2Findex.php%3Fthreads%2Fnew-teen-titans-tower-look.296433%2F&psig=AOvVaw0VUZ1umKrPFlebIBx6AdDT&ust=1553635326971397")
-	Night_Wing=(img = elemento_Night_Wing,
+	cenaTowertitan = Cena(img = "https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiuo7fonJ7hAhWFD7kGHfJ_ChYQjRx6BAgBEAU&url=http%3A%2F%2Fforums.daybreakgames.com%2Fdcuo%2Findex.php%3Fthreads%2Fnew-teen-titans-tower-look.296433%2F&psig=AOvVaw0VUZ1umKrPFlebIBx6AdDT&ust=1553635326971397")
+	night_wing=(img = elemento_night_wing,
                  tit="Night Wing one",
                  style = dict( left=100, top=60, width=60, heigt=150))
-	Night_Wing.entra(cenatowertitan)
-	txtNight_Wing = Texto (cenatowertitan,
+	night_wing.entra(cenaTowertitan)
+	txtNight_Wing = Texto (cenaTowertitan,
                              "Estou em casa de novo!")
-	Night_Wing.vai = txtNight_Wing.vai
-	cenatowertitan.vai()
+	night_wing.vai = txtNight_Wing.vai
+	cenaTowertitan.vai()
 Historia()
