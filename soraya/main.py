5c8948c379ff82d5568c80e07ba43296219e42d3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 18:27:55 -0300

    Saved soraya/main.py at Timestamp: 2019-04-17 21:27:55

--- soraya/main.py
+++ soraya/main.py
@@ -8,7 +8,7 @@ def Historia():
                       tit="Gato Laranja",
                       style = dict( left=150, top =60, width=60, height=200))
 	gato.entra(cenaHouse)
-	txtGato=Texto(cenaHouse
+	txtGato = Texto(cenaHouse,
                     "Estou com fome!")
 
 	gato.vai=txtGato.vai
