b7e3cc9bf77989e697d98e6e7207340d76ca2917
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 18:20:17 -0300

    Saved alexa/__log__.py at Timestamp: 2019-04-17 21:20:17

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -154,3 +154,10 @@ SyntaxError: invalid syntax
                                                                                                                    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Apr 17 2019 18:20:16.765 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  bart = Elemento (img = elemento_Bart,
+                                                                                                                   ^
+SyntaxError: invalid syntax
+'''},
