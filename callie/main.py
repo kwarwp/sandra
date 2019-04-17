d6d4546ac06f984d5834a69210ce1f454afab39d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 18:22:51 -0300

    Saved callie/main.py at Timestamp: 2019-04-17 21:22:50

--- callie/main.py
+++ callie/main.py
@@ -1,6 +1,6 @@
 # Pedro Henrique Barros	
 from _spy.vitollino.main import Cena, Elemento, Texto
-Elemento_Night_Wing="https://vignette.wikia.nocookie.net/nerdologiaheroespowers/images/2/23/Asa_Noturna.png/revision/latest?cb=20180206000240&path-prefix=pt-br"def Historia():
+elemento_Night_Wing="https://vignette.wikia.nocookie.net/nerdologiaheroespowers/images/2/23/Asa_Noturna.png/revision/latest?cb=20180206000240&path-prefix=pt-br"def Historia():
 
 def Historia():
 	cenatowertitan = Cena(img = "https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiuo7fonJ7hAhWFD7kGHfJ_ChYQjRx6BAgBEAU&url=http%3A%2F%2Fforums.daybreakgames.com%2Fdcuo%2Findex.php%3Fthreads%2Fnew-teen-titans-tower-look.296433%2F&psig=AOvVaw0VUZ1umKrPFlebIBx6AdDT&ust=1553635326971397")
@@ -8,5 +8,8 @@ def Historia():
                  tit="Night Wing one",
                  style = dict( left=100, top=60, width=60, heigt=150))
 	Night_Wing.entra(cenatowertitan)
-cenatowertitan.vai()
+	txtNight_Wing = Texto (cenatowertitan,
+                             "Estou em casa de novo!")
+	Night_Wing.vai = txtNight_Wing.vai
+	cenatowertitan.vai()
 Historia()
