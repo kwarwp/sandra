03b3c6695560e623c8ef4785295345259cb7e073
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 18:30:57 -0300

    Created samantha/__log__.py at Timestamp: 2019-04-17 21:30:57

---
+++ samantha/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Apr 17 2019 18:30:55.453 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  cenahouse.vai 
+  ^
+IndentationError: unexpected indent
+'''},
