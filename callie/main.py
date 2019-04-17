6c7d92907c8510ecdfa99cbb7f938100f9232e8f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 18:23:36 -0300

    Saved callie/main.py at Timestamp: 2019-04-17 21:23:35

--- callie/main.py
+++ callie/main.py
@@ -1,10 +1,9 @@
 # Pedro Henrique Barros	
 from _spy.vitollino.main import Cena, Elemento, Texto
-elemento_Night_Wing="https://vignette.wikia.nocookie.net/nerdologiaheroespowers/images/2/23/Asa_Noturna.png/revision/latest?cb=20180206000240&path-prefix=pt-br"def Historia():
-
+elemento_Night_Wing="https://vignette.wikia.nocookie.net/nerdologiaheroespowers/images/2/23/Asa_Noturna.png/revision/latest?cb=20180206000240&path-prefix=pt-br"
 def Historia():
 	cenatowertitan = Cena(img = "https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiuo7fonJ7hAhWFD7kGHfJ_ChYQjRx6BAgBEAU&url=http%3A%2F%2Fforums.daybreakgames.com%2Fdcuo%2Findex.php%3Fthreads%2Fnew-teen-titans-tower-look.296433%2F&psig=AOvVaw0VUZ1umKrPFlebIBx6AdDT&ust=1553635326971397")
-	Night_Wing=(img = Elemento_Night_Wing,
+	Night_Wing=(img = elemento_Night_Wing,
                  tit="Night Wing one",
                  style = dict( left=100, top=60, width=60, heigt=150))
 	Night_Wing.entra(cenatowertitan)
