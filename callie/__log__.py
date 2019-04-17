8c12e47d6ce37146a81f3515a43f5c2432c22733
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 18:19:09 -0300

    Saved callie/__log__.py at Timestamp: 2019-04-17 21:19:09

--- callie/__log__.py
+++ callie/__log__.py
@@ -174,3 +174,10 @@ SyntaxError: invalid syntax
                                                                                                                                                                    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Apr 17 2019 18:19:08.578 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  elemento_Night_Wing="https://vignette.wikia.nocookie.net/nerdologiaheroespowers/images/2/23/Asa_Noturna.png/revision/latest?cb=20180206000240&path-prefix=pt-br"def Historia():
+                                                                                                                                                                   ^
+SyntaxError: invalid syntax
+'''},
