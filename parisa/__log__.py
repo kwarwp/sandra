4bb067fbaa76354a24cfcdef969bde7c177185a7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 18:03:06 -0300

    Saved parisa/__log__.py at Timestamp: 2019-04-17 21:03:06

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -134,3 +134,10 @@ SyntaxError: invalid syntax
                                                                                                                                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Apr 17 2019 18:03:06.372 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  mickey = Elemento (img = elemento_mickey,
+                                                                                                                                                          ^
+SyntaxError: invalid syntax
+'''},
