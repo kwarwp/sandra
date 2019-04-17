859935db6d4c65740f313861715ba135fc84685b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-17 18:27:33 -0300

    Saved alexa/main.py at Timestamp: 2019-04-17 21:27:33

--- alexa/main.py
+++ alexa/main.py
@@ -1,6 +1,6 @@
 # jorge
 from _spy.vitollino.main import Cena, Elemento, Texto
-elemento_Bart = https://static.simpsonswiki.com/images/8/83/Bart_skate_-_s25_artwork.png
+elemento_Bart = "https://static.simpsonswiki.com/images/8/83/Bart_skate_-_s25_artwork.png"
 
 def Historia():
 	cenaHouse= Cena(img = "http://ossimpsons-orpg.weebly.com/uploads/1/2/9/6/12964529/850096305.jpg?554")    
