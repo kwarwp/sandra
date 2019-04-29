e840b68fc082ce7ebf6116ac4e300482cd734182
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-29 18:52:41 -0300

    Saved rachel/__log__.py at Timestamp: 2019-04-29 21:52:40

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -76,3 +76,21 @@ SyntaxError: EOL while scanning string literal
                                                                                          ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Mon Apr 29 2019 18:52:40.226 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 17
+    Historia()  
+  module <module> line 6
+    	cenaKaneki= Cena(img = "https://i.ytimg.com/vi/_-GI_EG7UWM/hqdefault.jpg")
+TypeError: 'module' object is not callable
+'''},
