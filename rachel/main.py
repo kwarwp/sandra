758b2b67652a59b7677328df39ad35cd324aa437
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-29 18:51:24 -0300

    Saved rachel/main.py at Timestamp: 2019-04-29 21:51:24

--- rachel/main.py
+++ rachel/main.py
@@ -1,9 +1,9 @@
 #danielmartins
 from _spy.vittolino.main import Cena, Elemento, Texto
-elemento_kaneki = "https://data.whicdn.com/images/204362721/superthumb.png?t=1445214745
+elemento_kaneki = "https://data.whicdn.com/images/204362721/superthumb.png?t=1445214745"
   
 def Historia():
-	cenaKaneki= Cena()
+	cenaKaneki= Cena(img = "https://i.ytimg.com/vi/_-GI_EG7UWM/hqdefault.jpg")
 	kaneki=Elemento(img=elemento_kaneki,
 tit="kaneki preto",
                       style=dict( left=150, top =60, width=60, height=200)) 
