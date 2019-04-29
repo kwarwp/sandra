25d85c2a8bd295c10030b36a151127900985f71e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-29 18:36:39 -0300

    Saved morgan/__log__.py at Timestamp: 2019-04-29 21:36:39

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -390,3 +390,21 @@ TypeError: 'module' object is not callable
     	cenaHouse= Cena(img = "http://farm3.static.flickr.com/2290/1888495823_ef459b5c3e.jpg")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Mon Apr 29 2019 18:36:39.272 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    Historia()
+  module <module> line 6
+    	cenaHouse= Cena(img = "http://farm3.static.flickr.com/2290/1888495823_ef459b5c3e.jpg")
+TypeError: 'module' object is not callable
+'''},
