b712dc65e5890af565e3a650a9e829a11cfd7409
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-29 18:43:51 -0300

    Saved alexa/__log__.py at Timestamp: 2019-04-29 21:43:51

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -210,3 +210,10 @@ SyntaxError: EOL while scanning string literal
                           ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Mon Apr 29 2019 18:43:49.282 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  "The Pirates Return Home
+                          ^
+SyntaxError: EOL while scanning string literal
+'''},
