b0926a774b802728fc8ba27b10e8f69942aafa08
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-29 18:43:48 -0300

    Saved alexa/__log__.py at Timestamp: 2019-04-29 21:43:48

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -196,3 +196,10 @@ SyntaxError: invalid syntax
                                                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Apr 29 2019 18:43:47.515 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  "The Pirates Return Home
+                          ^
+SyntaxError: EOL while scanning string literal
+'''},
