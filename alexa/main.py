d64af61a1ff6fa9c92939e106616efae4dc40e24
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-29 18:43:47 -0300

    Saved alexa/main.py at Timestamp: 2019-04-29 21:43:47

--- alexa/main.py
+++ alexa/main.py
@@ -9,10 +9,10 @@ def Historia():
                       style = dict(left=150,top=60,width=60, height=200))
 	bart.entra(cenaHouse) 
 	txtBart = Texto( cenaHouse,
-                 The Pirates Return Home
+                 "The Pirates Return Home
 	No domingo, a cidade foi para o funeral do três amigos, de repente eles apareceram na igreja todos sujos . Os parentes ficaram muito felizes e Amy também. Tom voltou a ir para a escola e nesse dia salvou sua amiga de apuros que iria entrar como o professor.
 
-On Sunday, the city went to the funeral of the three friends, suddenly they appeared in the church all dirty. The relatives were very happy and so was Amy. Tom went back to school and on that day saved his friend from trouble who would enter as the teacher.
+On Sunday, the city went to the funeral of the three friends, suddenly they appeared in the church all dirty. The relatives were very happy and so was Amy. Tom went back to school and on that day saved his friend from trouble who would enter as the teacher."
 )
 	bart.vai= txtBart.vai
 	cenaHouse.vai()                    
