64655deec61f3a937c75a0199db03167d8802d8e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-05-24 23:53:36 -0300

    Created stacy/__log__.py @2022-05-25 02:53

---
+++ stacy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue May 24 2022 23:53:34.285 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  elemento_mulhermaravilha = https://3.bp.blogspot.com/-mu96D51rmjY/WNgdie4l9iI/AAAAAAAAJDo/ZGMs0G1LvZABrLFaPn5exbhEOZ6twGc1wCLcB/s1600/justice_league___wonder_woman_png_by_mintmovi3-db3hh8l.png
+                                   ^
+SyntaxError: invalid syntax
+'''},
