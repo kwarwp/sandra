b19205ea8e5c9b9acc7bcbadc94772286608f0a8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-05-24 23:54:29 -0300

    Created sara/__log__.py @2022-05-25 02:54

---
+++ sara/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue May 24 2022 23:54:27.124 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  elemento-sou luna /https://encrypted-tbn0.gstatic.com/images?q=tbn:
+                ^
+SyntaxError: invalid syntax
+'''},
